69fed9b18e0722bcc55ca43d27e95fa5e6d623cf
Ridolfi0 <nicolo.ridolfi.stud@ispascalcomandini.it>
2025-10-23 13:58:15 +0200

    new files

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+# from app.models import Driver
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello_world():
     return "Hello World"
 
+@app.route("/about") # ad esempio ne possiamo creare una per driver, passeggero...
+def about():
+    return "About this page"
+
+app.route("/user/<username>") # è possibile attrinuire pure un tipo ==> app.route("/user/<str:username>")
+def show_user_profile(username):
+    return f"User: {username}"
+
+
 if __name__ == "__main__":
     app.run()
+
