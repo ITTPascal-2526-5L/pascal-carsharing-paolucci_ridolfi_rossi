d8cdaf16c83cead4165702bb61f5b92d9478aca9
paolucciD <deniel.paolucci.stud@ispascalcomandini.it>
2025-10-29 13:55:00 +0100

    Inizio Interfaccia registrazione

--- app/routes/registration.py
+++ app/routes/registration.py
@@ -4,11 +4,11 @@ registration_bp = Blueprint("registration", __name__)
 
 @registration_bp.route("/registration_driver")
 def registration_driver():
-    return render_template("index.html")
+    return render_template("registration_d.html")
 
 @registration_bp.route("/registration_passenger")
 def registration_passenger():
-    return render_template("index.html")
+    return render_template("registration_p.html")
 
 #Tutti i blueprints devono essere registrati nella creazione dell'applicativo
 
