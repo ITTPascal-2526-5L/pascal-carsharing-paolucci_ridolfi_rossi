ec13fac3484ff626947b0d00266130a73973c92a
Ridolfi0 <nicolo.ridolfi.stud@ispascalcomandini.it>
2026-01-07 21:18:45 +0100

    Piccole modifiche e creazione dei metodi get per richiamare le varie classi.

--- app/models/post.py
+++ app/models/post.py
@@ -1,8 +1,11 @@
-from app import db
-from datetime import datetime
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+# from app import db
+# from datetime import datetime
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return f'<Post {self.id} - Title: {self.title} - User ID: {self.user_id} - Date Posted: {self.date_posted}>'
